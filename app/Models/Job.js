3a8e385bcf052cba9426fa7018aff9b48786a178
Jorge Ribeiro <joorgemelo@gmail.com>
2018-10-24 08:48:35 -0300

    new file:   app/Controllers/Http/JobController.js 	modified:   app/Models/Job.js 	modified:   start/routes.js

--- app/Models/Job.js
+++ app/Models/Job.js
@@ -1,9 +1,8 @@
-'use strict'
+"use strict";
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
-class Job extends Model {
-}
+class Job extends Model {}
 
-module.exports = Job
+module.exports = Job;
