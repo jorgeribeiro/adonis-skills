0ba9293cb9df5c9951331dc718562641dda1479b
Jorge Ribeiro <joorgemelo@gmail.com>
2018-11-12 08:22:48 -0300

    modified:   app/Controllers/Http/UserController.js 	modified:   app/Validators/CreateUser.js 	new file:   app/Validators/LoginUser.js 	modified:   resources/views/auth/login.edge 	modified:   resources/views/auth/signup.edge 	modified:   start/routes.js

--- start/routes.js
+++ start/routes.js
@@ -22,6 +22,7 @@ Route.on("/signup").render("auth.signup");
 Route.on("/login").render("auth.login");
 
 Route.post("/signup", "UserController.create").validator("CreateUser");
+Route.post("/login", "UserController.login").validator("LoginUser");
 
 Route.get("/logout", async ({ auth, response }) => {
   await auth.logout();
